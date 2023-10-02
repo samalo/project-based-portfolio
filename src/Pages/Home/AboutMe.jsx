854aaee3f932652d33c7bcec219acc246c7410b5
Sam Mhondiwa <smhonds@gmail.com>
2023-10-02 23:56:13 +0200

    Adding About Section

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,27 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills--section--header">About Me</h1>
+          <p className="hero--section-description">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book
+          </p>
+          <p className="hero--section-description">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,10 +1,12 @@
 import HeroSection from "../HeroSection";
 import MySkills from "../MySkills";
+import AboutMe from "../AboutMe";
 export default function Home() {
   return (
     <>
       <HeroSection />
       <MySkills />
+      <AboutMe />
     </>
   );
 }
